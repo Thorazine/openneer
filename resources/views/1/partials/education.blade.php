073a7769f1c86cdfdf3bbe36696718141ac50efe
Matthijs Openneer <matthijs@noprotocol.nl>
2017-06-28 16:04:01 +0200

    solved some more crap

--- resources/views/1/partials/education.blade.php
+++ resources/views/1/partials/education.blade.php
@@ -1,19 +1,10 @@
 <div class="education-item">
 	<div class="timeline">
-<<<<<<< HEAD
 		{{ $education->year }}
 		<div class="line"></div>
 	</div>
 	<div class="description">
 		<h3>{{ $education->place }}</h3>
 		{!! $education->description !!}
-=======
-		1995
-		<div class="line"></div>
-	</div>
-	<div class="description">
-		<h3>MAVO</h3>
-		<p>Nieuwer Amstel en Amstelhoven</p>
->>>>>>> f52a012c579fbe7c28aa17f92045921a5870f450
 	</div>
 </div>
