bdf7023feca745f241082e5639351b0980b58670
Matthijs Openneer <matthijs@noprotocol.nl>
2017-07-07 13:06:16 +0200

    updated hack to 1.0.30

---
+++ resources/views/1/language.blade.php
@@ -0,0 +1,29 @@
+@extends('1.layouts.default')
+
+
+
+@section('content')
+	
+	<div id="content" class="skill">
+		<div id="wrapper-content">
+			<h1>Pick a language</h1>
+
+			<div class="grid">
+				<div class="grid-6">
+					<img src="{{ asset('flags/en.png') }}">
+					<a href="{{ route('page', ['slug' => 'en']) }}">English</a>
+				</div>
+				<div class="grid-6">
+					<img src="{{ asset('flags/nl.png') }}">
+					<a href="{{ route('page', ['slug' => 'nl']) }}">Nederlands</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+@stop
+
+
+@section('script')
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+@stop
